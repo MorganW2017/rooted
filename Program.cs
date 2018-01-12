aef55f655e49cf0150bc5696f0bf7b21a7163166
Morgan Waters <morangwaters@gmail.com>
2018-01-12 11:11:57 -0700

    setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rooted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
